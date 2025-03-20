15ed31dd76ebb107939b7d0a39154780507f862c
oHastee <oscarpiediaz@gmail.com>
2025-03-19 23:19:30 -0400

    Updates

---
+++ precache-manifest.3e8b359c9e6c91468c1ae7a4805bfd02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcefb2a8ab46ae41c47ac834ead2ce5",
+    "url": "/OscarPiedrasantaDiazPortfolio/index.html"
+  },
+  {
+    "revision": "b73d47148a2fb6e56fa2",
+    "url": "/OscarPiedrasantaDiazPortfolio/static/css/2.ee6fb1dc.chunk.css"
+  },
+  {
+    "revision": "3028504794565fabe9b6",
+    "url": "/OscarPiedrasantaDiazPortfolio/static/css/main.8149ebb8.chunk.css"
+  },
+  {
+    "revision": "b73d47148a2fb6e56fa2",
+    "url": "/OscarPiedrasantaDiazPortfolio/static/js/2.342bcaac.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/OscarPiedrasantaDiazPortfolio/static/js/2.342bcaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3028504794565fabe9b6",
+    "url": "/OscarPiedrasantaDiazPortfolio/static/js/main.1ef597d3.chunk.js"
+  },
+  {
+    "revision": "aa2b70ba91f405203636",
+    "url": "/OscarPiedrasantaDiazPortfolio/static/js/runtime-main.8a068f4c.js"
+  }
+]);
